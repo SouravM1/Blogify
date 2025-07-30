16a13e76500e9a95a509366ec9ba9dee9879796e
Sourav Mukherjee <22souravmukherjee@gmail.com>
2025-07-30 12:02:37 +0530

    Fix login 404 error by correcting AuthLayout authentication logic

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -2,26 +2,19 @@ import React, {useEffect, useState} from 'react'
 import {useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
-export default function Protected({children, authentication = true}) {
+export default function AuthLayout({children, authentication = true}) {
 
     const navigate = useNavigate()
     const [loader, setLoader] = useState(true)
     const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
-        //TODO: make it more easy to understand
-
-        // if (authStatus ===true){
-        //     navigate("/")
-        // } else if (authStatus === false) {
-        //     navigate("/login")
-        // }
-        
-        //let authValue = authStatus === true ? true : false
-
-        if(authentication && authStatus !== authentication){
+        // If authentication is required and user is not authenticated, redirect to login
+        if(authentication && !authStatus){
             navigate("/login")
-        } else if(!authentication && authStatus !== authentication){
+        } 
+        // If authentication is NOT required (like login/signup pages) and user IS authenticated, redirect to home
+        else if(!authentication && authStatus){
             navigate("/")
         }
         setLoader(false)
